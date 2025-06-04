61a495e337b1590f39a4fcc0eea5fee839cdfd0e
thowfee-md <thafithowfee@gmail.com>
2025-06-04 21:05:27 +0530

    Add files via upload

---
+++ Tools.bash
@@ -0,0 +1,15 @@
+MINICONDA  - wget https://repo.anaconda.com/miniconda/Miniconda3 latest-Linux-x86_64.sh bash Miniconda3-latest-Linux-x86_64.sh 
+ 
+FASTQC - conda install –c bioconda  fastqc 
+ 
+FASTP - conda install –c bioconda  fastp 
+ 
+BWA - conda install –c bioconda  bwa 
+
+SAMTOOLS - conda install –c bioconda  samtools 
+
+PICARD - conda install -c bioconda picard 
+ 
+FREEBAYES - conda install -c bioconda  free bayes 
+ 
+GATK4 - conda install -c bioconda gatk4
